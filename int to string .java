1949386acd82024f8af87dc4ccc403e083dd2c24
Tamilselvi <112626100+tamilselvi03@users.noreply.github.com>
2022-11-06 13:49:38 +0530

    Create int to string .java

---
+++ int to string .java
@@ -0,0 +1,54 @@
+You are given an integer n, you have to convert it into a string.
+ Please complete the partially completed code in the editor. If your code successfully
+ converts n into a string S the code will print "Good job". Otherwise it will print "Wrong
+ answer".
+ n can range between -100 to 100 inclusive.
+ Sample Input 0
+    100
+ Sample Output 0
+    Good job
+
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
+        convert(num);
+    }
+        static void convert(int n){
+            if(n>=-100&&n<=100){
+            Integer. toString(n);
+            System.out.println("Good job");
+        }
+        else{
+            System.out.println("Wrong answer");
+        }
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    
+}
+}import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
+        convert(num);
+    }
+        static void convert(int n){
+            if(n>=-100&&n<=100){
+            Integer. toString(n);
+            System.out.println("Good job");
+        }
+        else{
+            System.out.println("Wrong answer");
+        }
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    
+}
+}
